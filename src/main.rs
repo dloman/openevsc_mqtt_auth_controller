50d7b5bd85998f75ce71fbcf21d292e7d36e9cff
Dan Loman <daniel.loman@getcruise.com>
2024-10-06 15:56:56 -0700

    added braintree

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,54 @@ use std::io::{BufReader, BufWriter, Write};
 use std::ops::Mul;
 use std::str::FromStr;
 extern crate pretty_env_logger;
+extern crate braintree;
 #[macro_use] extern crate log;
 extern crate derive_more;
-use derive_more::{Sub, AddAssign, SubAssign};
+use derive_more::{Sub, AddAssign, SubAssign, Add, Into};
+
+use braintree::{Braintree, Environment};
+use braintree::transaction;
 
 const RFID: &str = "evcharger1/rfid_auth";
 const OVERRIDE: &str = "evcharger1/override";
 const ENERGY: &str = "evcharger1/session_energy";
 const VEHICLE: &str = "evcharger1/vehicle";
+
+fn print_transaction(transaction: transaction::Transaction) {
+    info!("        ID: {}", transaction.id);
+    info!("      Type: {:?}", transaction.typ);
+    info!("    Amount: {}", transaction.amount);
+    info!("  Currency: {}", transaction.currency_iso_code);
+    info!("    Status: {:?}", transaction.status);
+    info!("       URL: https://sandbox.braintreegateway.com/merchants/MERCHANT_ID/transactions/{}", transaction.id);
+}
+
+fn charge_user(mut user : User, bt : &Braintree) -> Result<User, braintree::Error> {
+    let amount = Dollars(100.0);
+    let result = bt.transaction().create(transaction::Request{
+        amount: amount.0.to_string(),
+        customer_id: Some(user.customer_id.clone()),
+        options: Some(braintree::transaction::Options{
+            submit_for_settlement: Some(true),
+            ..Default::default()
+        }),
+        ..Default::default()
+    });
+
+    match result {
+                Ok(transaction) => {
+                    user.dollars_remaining += amount;
+                    info!("\n  Successfully created a transaction! Details to follow:\n");
+                    print_transaction(transaction);
+                    info!("");
+                    return Ok(user);
+                },
+                Err(err) => {
+                    error!("\nError: {}\n", err);
+                    return Err(err);
+                }
+            }
+}
 
 fn handle_packet(notification : Result<Event, ConnectionError>) -> (String, Bytes) {
     if let Ok(event) = notification {
@@ -36,7 +76,7 @@ struct KWh(f64);
 #[derive(Serialize, Deserialize, Debug)]
 struct DollarsPerkWh(f64);
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default, AddAssign, SubAssign)]
+#[derive(Serialize, Deserialize, Debug, Clone, Default, AddAssign, SubAssign, Add, PartialEq, PartialOrd, Into)]
 struct Dollars(f64);
 
 impl Mul<DollarsPerkWh> for KWh {
@@ -58,6 +98,7 @@ struct User {
     rfid: String,
     dollars_remaining: Dollars,
     total_lifetime_usage: Usage,
+    customer_id: String,
 }
 
 
@@ -145,10 +186,10 @@ fn handle_override(payload : &Bytes) {
     }
 }
 
-fn handle_energy(payload : &Bytes, current_session: Option<Session>) -> Option<Session> {
+fn handle_energy(payload : &Bytes, current_session: Option<Session>, client: &Client, bt : &Braintree) -> Option<Session> {
     match std::str::from_utf8(&payload) {
         Ok(watt_hours) => {
-            return add_energy_to_session(&watt_hours, current_session);
+            return add_energy_to_session(&watt_hours, current_session, client, bt);
         },
         Err(e) => {
             error!("{}", e);
@@ -166,7 +207,7 @@ fn get_usage(update : KWh, mut usage : Usage, current_hour : u32) -> Usage {
     return usage;
 }
 
-fn add_energy_to_session(watt_hours: &str, current_session: Option<Session>)  -> Option<Session> {
+fn add_energy_to_session(watt_hours: &str, current_session: Option<Session>, client: &Client, bt : &Braintree)  -> Option<Session> {
     match f64::from_str(watt_hours) {
         Ok(watt_hours) => {
             let kw_hours = KWh(watt_hours/1000.0);
@@ -174,9 +215,19 @@ fn add_energy_to_session(watt_hours: &str, current_session: Option<Session>)  ->
             match current_session {
                 Some(mut session) => {
                     let current_hour = chrono::offset::Local::now().time().hour();
-                    info!("current time is {}", current_hour);
 
-                    session.usage = get_usage(kw_hours.clone() - session.kwh_used, session.usage, current_hour);
+                    session.usage = get_usage(kw_hours.clone() - session.kwh_used.clone(), session.usage.clone(), current_hour);
+                    info!("current cost is {:#?} remaining money is {:#?}", compute_cost(session.usage.clone()), session.user.dollars_remaining);
+                    if session.user.dollars_remaining <= compute_cost(session.usage.clone()) {
+                        match charge_user(session.user.clone(), bt) {
+                            Ok(charged_user) => { session.user = charged_user; }
+                            Err(e) => {
+                                error!("charing failed disabling {:?}", e);
+                                send_override(State::Disabled, client);
+                                return Some(session); // am i removing dollars from users?
+                            }
+                        }
+                    }
                     session.kwh_used = kw_hours;
                     return Some(session);
                 },
@@ -190,11 +241,22 @@ fn add_energy_to_session(watt_hours: &str, current_session: Option<Session>)  ->
     }
 }
 
-fn update_user(mut user: User, usage : Usage) -> User {
-    let mut dollars_used = usage.on_peak.clone() * RATES.on_peak.price_per_kwh;
-    dollars_used += usage.off_peak.clone() * RATES.off_peak.price_per_kwh;
+fn compute_cost(usage : Usage) -> Dollars {
+    let dollars_used = usage.on_peak.clone() * RATES.on_peak.price_per_kwh;
+    return dollars_used + usage.off_peak.clone() * RATES.off_peak.price_per_kwh
+}
+
+fn update_user(mut user: User, usage : Usage, bt : &Braintree) -> User {
+    let dollars_used = compute_cost(usage.clone());
     info!("removing current session {:?} from user {} balance of {:?} ", dollars_used, user.name, user.dollars_remaining);
     user.dollars_remaining -= dollars_used;
+    if user.dollars_remaining < Dollars(0.0) {
+        debug!("charging user {:?} {:?} ", usage, user);
+        match charge_user(user.clone(), bt) {
+            Ok(charged_user) => user = charged_user,
+            Err(e) => { error!("charge failed: {}", e); }
+        }
+    }
     user.total_lifetime_usage += usage;
     info!("new user = {:?}", user.clone());
     return user;
@@ -219,7 +281,7 @@ fn write_user_json(users:&mut BTreeMap<String, User>) {
     }
 }
 
-fn handle_vehicle(payload : &Bytes, current_session: Option<Session>, users: &mut BTreeMap<String, User>, client: &Client) -> Option<Session> {
+fn handle_vehicle(payload : &Bytes, current_session: Option<Session>, users: &mut BTreeMap<String, User>, client: &Client, bt : &Braintree) -> Option<Session> {
     match std::str::from_utf8(&payload) {
         Ok(is_connected) => {
             let is_connected = is_connected == "1";
@@ -235,7 +297,7 @@ fn handle_vehicle(payload : &Bytes, current_session: Option<Session>, users: &mu
                     }
 
                     send_override(State::Disabled, client);
-                    let user = update_user(session.user, session.usage);
+                    let user = update_user(session.user, session.usage, bt);
                     users.insert(user.rfid.clone(), user);
                     write_user_json(users);
                     return None;
@@ -253,6 +315,14 @@ fn handle_vehicle(payload : &Bytes, current_session: Option<Session>, users: &mu
 fn main() -> std::io::Result<()> {
     // Initialize the logger
     pretty_env_logger::init();
+
+    let merchant_id = std::env::var("MERCHANT_ID").expect("environment variable MERCHANT_ID is not defined");
+    let bt = Braintree::new(
+        Environment::Sandbox,
+        merchant_id.clone(),
+        std::env::var("PUBLIC_KEY").expect("environment variable PUBLIC_KEY is not defined"),
+        std::env::var("PRIVATE_KEY").expect("environment variable PRIVATE_KEY is not defined"),
+    );
 
     let file = File::open("users.json".to_string())?;
     let reader = BufReader::new(file);
@@ -281,9 +351,9 @@ fn main() -> std::io::Result<()> {
 
         match topic.as_str() {
             RFID  => { current_session = handle_rfid(&payload, current_session, &client, &users); },
-            ENERGY  => { current_session = handle_energy(&payload, current_session); },
+            ENERGY  => { current_session = handle_energy(&payload, current_session, &client, &bt); },
             OVERRIDE  => { handle_override(&payload); },
-            VEHICLE  => { current_session = handle_vehicle(&payload, current_session, &mut users, &client); },
+            VEHICLE  => { current_session = handle_vehicle(&payload, current_session, &mut users, &client, &bt); },
             _ => { }
         }
     }
